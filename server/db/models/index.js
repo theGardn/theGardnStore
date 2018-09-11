7898158efcef5738a7e6d6b99ece81a44e856290
johnlatona <johnbl24@aol.com>
2018-09-11 16:08:44 -0400

    Database #Associations - created model associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,21 +1,25 @@
 const User = require('./user')
 const Product = require('./product')
-const Category = require('./category');
-const Order_Detail = require('./order_detail');
-const Order = require('./order');
+const Category = require('./category')
+const Order_Detail = require('./order_detail')
+const Order = require('./order')
 
-Product.belongsTo(Category);
-Category.hasMany(Product);
+Product.belongsTo(Category)
+Category.hasMany(Product)
 
-Order_Detail.belongsTo(Product);
-Product.hasMany(Order_Detail);
+Order_Detail.belongsTo(Product)
+Product.hasMany(Order_Detail)
 
-Order.belongsTo(User);
-User.hasMany(Order);
+Order.belongsTo(User)
+User.hasMany(Order)
 
-Order_Detail.belongsTo(Order);
-Order.hasMany(Order_Detail);
+Order_Detail.belongsTo(Order)
+Order.hasMany(Order_Detail)
 
 module.exports = {
-  User, Product, Category, Order, Order_Detail
+  User,
+  Product,
+  Category,
+  Order,
+  Order_Detail
 }
