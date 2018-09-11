ea6889749c8f013e6a7570cab51978271e3aa8d1
Peng <admin@AC02RK0A3GL27.local>
2018-09-11 16:30:09 -0400

    CreateDatabse #productdb prettier

--- server/db/models/orderDetails.js
+++ server/db/models/orderDetails.js
@@ -2,18 +2,18 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order_Detail = db.define('order_details', {
-    quantity: {
-        type: Sequelize.INTEGER,
-        validate: {
-            min: 0
-        }
-      },
-    price: {
-    type: Sequelize.DECIMAL(10, 2),
-    allowNull:false,
+  quantity: {
+    type: Sequelize.INTEGER,
     validate: {
-        notEmpty: true,
-        min: 0
+      min: 0
+    }
+  },
+  price: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      min: 0
     }
   }
 })
