4efff9ac2a66d7cb9fab6a56cc67e8dd404099c9
Ryan McNierney <ryan.mcnierney@gmail.com>
2018-09-11 17:19:00 -0400

    Database #Seed-file - ran prettify

--- script/seed.js
+++ script/seed.js
@@ -1,10 +1,16 @@
 'use strict'
 
 const db = require('../server/db')
-const { User, Product, Category, Order, Order_Detail } = require('../server/db/models')
+const {
+  User,
+  Product,
+  Category,
+  Order,
+  Order_Detail
+} = require('../server/db/models')
 
 async function seed() {
-  await db.sync({ force: true })
+  await db.sync({force: true})
   console.log('db synced!')
 
   const users = await Promise.all([
@@ -13,28 +19,28 @@ async function seed() {
       lastName: 'Pug',
       email: 'cody@email.com',
       password: '123',
-      isLoggedIn: false,
+      isLoggedIn: false
     }),
     User.create({
       firstName: 'Murphy',
       lastName: 'FullStack',
       email: 'murphy@email.com',
       password: '123',
-      isLoggedIn: false,
+      isLoggedIn: false
     }),
     User.create({
       firstName: 'Ryan',
       lastName: 'McNierney',
       email: 'ryan@email.com',
       password: '123',
-      isLoggedIn: false,
+      isLoggedIn: false
     }),
     User.create({
       firstName: 'Aaron',
       lastName: 'Ludwin',
       email: 'aaron@email.com',
       password: '123',
-      isLoggedIn: false,
+      isLoggedIn: false
     })
   ])
 
@@ -44,12 +50,14 @@ async function seed() {
   const categories = await Promise.all([
     Category.create({
       name: 'fruit',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Culinary_fruits_front_view.jpg/440px-Culinary_fruits_front_view.jpg'
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Culinary_fruits_front_view.jpg/440px-Culinary_fruits_front_view.jpg'
     }),
     Category.create({
       name: 'vegetable',
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Marketvegetables.jpg/440px-Marketvegetables.jpg'
-    }),
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/Marketvegetables.jpg/440px-Marketvegetables.jpg'
+    })
   ])
 
   console.log(`seeded ${categories.length} categories`)
@@ -59,72 +67,82 @@ async function seed() {
     Product.create({
       name: 'apples',
       categoryId: 1,
-      price: 1.00,
+      price: 1.0,
       quantity: 10,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Honeycrisp.jpg/440px-Honeycrisp.jpg'
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Honeycrisp.jpg/440px-Honeycrisp.jpg'
     }),
     Product.create({
       name: 'bananas',
       categoryId: 1,
-      price: 0.60,
+      price: 0.6,
       quantity: 20,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Banana_and_cross_section.jpg/500px-Banana_and_cross_section.jpg'
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Banana_and_cross_section.jpg/500px-Banana_and_cross_section.jpg'
     }),
     Product.create({
       name: 'grapes',
       categoryId: 1,
-      price: 1.50,
+      price: 1.5,
       quantity: 40,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Abhar-iran.JPG/340px-Abhar-iran.JPG'
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Abhar-iran.JPG/340px-Abhar-iran.JPG'
     }),
     Product.create({
       name: 'oranges',
       categoryId: 1,
-      price: 2.00,
+      price: 2.0,
       quantity: 25,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Orange-Whole-%26-Split.jpg/440px-Orange-Whole-%26-Split.jpg'
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Orange-Whole-%26-Split.jpg/440px-Orange-Whole-%26-Split.jpg'
     }),
     Product.create({
       name: 'carrots',
       categoryId: 2,
       price: 1.25,
       quantity: 18,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/7carrots.jpg/250px-7carrots.jpg'
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/7carrots.jpg/250px-7carrots.jpg'
     }),
     Product.create({
       name: 'tomatoes',
       categoryId: 2,
       price: 2.25,
       quantity: 26,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Tomato_je.jpg/250px-Tomato_je.jpg'
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Tomato_je.jpg/250px-Tomato_je.jpg'
     }),
     Product.create({
       name: 'cucumbers',
       categoryId: 2,
       price: 3.15,
       quantity: 14,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Og%C3%B3rki...jpg/250px-Og%C3%B3rki...jpg'
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Og%C3%B3rki...jpg/250px-Og%C3%B3rki...jpg'
     }),
     Product.create({
       name: 'onions',
       categoryId: 2,
       price: 2.75,
       quantity: 22,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Garlic.jpg/250px-Garlic.jpg'
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Garlic.jpg/250px-Garlic.jpg'
     }),
     Product.create({
       name: 'peppers',
       categoryId: 2,
-      price: 3.50,
+      price: 3.5,
       quantity: 35,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Red_capsicum_and_cross_section.jpg/250px-Red_capsicum_and_cross_section.jpg'
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Red_capsicum_and_cross_section.jpg/250px-Red_capsicum_and_cross_section.jpg'
     }),
     Product.create({
       name: 'lettuce',
       categoryId: 2,
       price: 1.15,
       quantity: 50,
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Kropsla_herfst.jpg/250px-Kropsla_herfst.jpg'
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Kropsla_herfst.jpg/250px-Kropsla_herfst.jpg'
     })
   ])
 
@@ -155,19 +173,19 @@ async function seed() {
     Order_Detail.create({
       productId: 1,
       quantity: 2,
-      price: 1.00,
+      price: 1.0,
       orderId: 1
     }),
     Order_Detail.create({
       productId: 2,
       quantity: 3,
-      price: 0.60,
+      price: 0.6,
       orderId: 1
     }),
     Order_Detail.create({
       productId: 3,
       quantity: 10,
-      price: 1.50,
+      price: 1.5,
       orderId: 1
     }),
     Order_Detail.create({
@@ -179,13 +197,13 @@ async function seed() {
     Order_Detail.create({
       productId: 4,
       quantity: 5,
-      price: 2.00,
+      price: 2.0,
       orderId: 1
     }),
     Order_Detail.create({
       productId: 9,
       quantity: 10,
-      price: 3.50,
+      price: 3.5,
       orderId: 2
     }),
     Order_Detail.create({
@@ -197,7 +215,7 @@ async function seed() {
     Order_Detail.create({
       productId: 1,
       quantity: 5,
-      price: 1.00,
+      price: 1.0,
       orderId: 2
     }),
     Order_Detail.create({
@@ -209,7 +227,7 @@ async function seed() {
     Order_Detail.create({
       productId: 3,
       quantity: 6,
-      price: 1.50,
+      price: 1.5,
       orderId: 3
     }),
     Order_Detail.create({
@@ -221,7 +239,7 @@ async function seed() {
     Order_Detail.create({
       productId: 2,
       quantity: 5,
-      price: 0.60,
+      price: 0.6,
       orderId: 3
     }),
     Order_Detail.create({
@@ -234,7 +252,6 @@ async function seed() {
 
   console.log(`seeded ${orderDetails.length} order details`)
   console.log(`seeded successfully`)
-
 }
 
 // We've separated the `seed` function from the `runSeed` function.
