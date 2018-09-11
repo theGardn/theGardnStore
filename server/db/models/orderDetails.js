15226b23838d4a172a9b026e4899cbdefd6a83d1
Peng <admin@AC02RK0A3GL27.local>
2018-09-11 16:18:24 -0400

    Database #productdb create models Product, Order_Detail

---
+++ server/db/models/orderDetails.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Order_Detail = db.define('order_details', {
+    quantity: {
+        type: Sequelize.INTEGER,
+        validate: {
+            min: 0
+        }
+      },
+    price: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull:false,
+    validate: {
+        notEmpty: true,
+        min: 0
+    }
+  }
+})
+
+module.exports = Order_Detail
---
+++ server/db/models/product.js
@@ -0,0 +1,51 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+        notEmpty: true
+    }
+  },
+  price: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull:false,
+    validate: {
+        notEmpty: true,
+        min: 0
+    }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    validate: {
+        min: 0
+    }
+  },
+  imageUrl: {
+      type: Sequelize.STRING,
+      defaultValue: 'productDefault.png'
+  }
+})
+
+module.exports = Product
+
+/**
+ * instanceMethods
+ */
+Product.prototype.getProductQuantity = function(getQuantity) {
+  if(getQuantity >= 0){
+    this.quantity -= getQuantity
+  }
+  return this.quantity
+}
+
+Product.prototype.addProductQuantity = function(addQuantity) {
+    if(addQuantity >= 0) {
+        this.quantity += addQuantity
+    }
+    return this.quantity
+  }
+
