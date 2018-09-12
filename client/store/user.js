fe89c603095ed0f23ca8acf7bd10d3af313dfd78
Peng <admin@AC02RK0A3GL27.local>
2018-09-12 18:17:08 -0400

    STORE #productdb -create userReducer with actions and thunk

--- client/store/user.js
+++ client/store/user.js
@@ -6,17 +6,31 @@ import history from '../history'
  */
 const GET_USER = 'GET_USER'
 const REMOVE_USER = 'REMOVE_USER'
+const ADD_ITEM_TO_CART = 'ADD_ITEM_TO_CART'
+const GOT_ITEM_TO_CART = 'GOT_ITEM_TO_CART'
+const REMOVE_ITEM_FROM_CART = 'REMOVE_ITEM_FROM_CART'
+const UPDATE_ITEM_IN_CART = 'UPDATE_ITEM_IN_CART'
+const GET_ORDER_HISTORY = 'GET_ORDER_HISTORY'
 
 /**
  * INITIAL STATE
  */
-const defaultUser = {}
+const defaultUser = {
+  firstName: '',
+  lastName: '',
+  cart: [],
+  orderHistory: []
+}
 
 /**
  * ACTION CREATORS
  */
 const getUser = user => ({type: GET_USER, user})
 const removeUser = () => ({type: REMOVE_USER})
+const addItemToCart = item => ({type: ADD_ITEM_TO_CART, item})
+const updateItemInCart = (...itemAndQuantity) => ({type: UPDATE_ITEM_IN_CART, itemAndQuantity})
+const removeItemFromCart = item => ({type: REMOVE_ITEM_FROM_CART, item})
+const getOrderHistory = orderHistory => ({type: GET_ORDER_HISTORY, orderHistory})
 
 /**
  * THUNK CREATORS
@@ -56,6 +70,46 @@ export const logout = () => async dispatch => {
   }
 }
 
+export const addItemToCartThunk = (item) => async dispatch => {
+  try {
+    const res = await axios.post('/api/cart', item)
+    dispatch(addItemToCart(res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const updateItemInCartThunk = (itemDetail) => async dispatch => {
+  try {
+    const res = await axios.put('/api/cart', itemDetail)
+    dispatch(updateItemInCart(res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const removeItemFromCartThunk = (item) => async dispatch => {
+  try {
+    const res = await axios.delete('/api/cart', item)
+    dispatch(removeItemFromCart(item))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const getOrderHistoryThunk = (orderId) => async dispatch => {
+  try {
+    if(orderId){
+      const res = await axios.get(`/oder/${orderId}`)
+    } else {
+      const res = await axios.get('/oder')
+    }
+    dispatch(getOrderHistory(res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
 /**
  * REDUCER
  */
@@ -65,6 +119,24 @@ export default function(state = defaultUser, action) {
       return action.user
     case REMOVE_USER:
       return defaultUser
+    case ADD_ITEM_TO_CART:
+      return {...state, cart: [...cart, action.item]}
+    case REMOVE_ITEM_FROM_CART:
+      let newCart = cart.filter((item) => {
+        return item.id != action.item.id
+      })
+      return {...state, cart: newCart}
+    case UPDATE_ITEM_IN_CART:
+      let newCart = cart.map((item) => {
+        if(item.id === action.item.id){
+          return {...item, quantity: action.item.quantity}
+        } else {
+          return item
+        }
+      })
+      return {...state, cart: newCart}
+    case GET_ORDER_HISTORY:
+      return {...state, orderHistory: action.orderHistory}
     default:
       return state
   }
