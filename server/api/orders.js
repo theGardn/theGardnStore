10bada8844787a807cc8cb86001ecd7d2c278a8e
Ryan McNierney <ryan.mcnierney@gmail.com>
2018-09-12 21:40:17 -0400

    Order by id #Cart-Routes - added GET route to pull order by id

---
+++ server/api/orders.js
@@ -0,0 +1,16 @@
+const router = require('express').Router()
+const { Order, Order_Detail } = require('../db/models')
+
+router.get('/:orderId', async (req, res, next) => {
+  try {
+    const orderId = req.params.orderId
+    const order = await Order.findById(orderId, {
+      include: [{ model: Order_Detail }]
+    })
+    res.status(200).json(order)
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
