e092f970937005f31d1787d02a4e5101ea3b68e4
Ryan McNierney <ryan.mcnierney@gmail.com>
2018-09-13 19:53:33 -0400

    Cart #Cart - added connect to map cart state

--- client/components/cart/cart-detail.js
+++ client/components/cart/cart-detail.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class CartDetail extends Component {
   render() {
+    const { cart } = this.props
+    console.log('cart', cart)
     return (
       <div id="cart-detail">
         <h2>Your Cart</h2>
@@ -11,8 +14,10 @@ class CartDetail extends Component {
 }
 
 const mapState = state => {
-
+  return {
+    cart: state.user.cart
+  }
 }
 
-export default CartDetail
+export default connect(mapState)(CartDetail)
 
