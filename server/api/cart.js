4ec36b91d29a5b4a65f00417ccf914de48a9e3fd
Ryan McNierney <ryan.mcnierney@gmail.com>
2018-09-13 13:49:38 -0400

    Private Routes #Cart-Routes - added destroy / put route on cart

--- server/api/cart.js
+++ server/api/cart.js
@@ -2,12 +2,15 @@ const router = require('express').Router()
 const { Order, Order_Detail } = require('../db/models')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+router.get('/:userId', async (req, res, next) => {
   try {
-    if (req.user) {
+    const sessionId = req.user.id
+    const userId = req.params.userId
+    if (sessionId === userId) {
       const cartItems = await Order.findOne({
         where: {
-          userId: req.user.id
+          userId: req.user.id,
+          purchased: false
         },
         include: [{ model: Order_Detail }]
       })
@@ -20,14 +23,16 @@ router.get('/', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   try {
-    if (req.user) {
+    const sessionId = req.user.id
+    const userId = req.body.userId
+    if (sessionId === userId) {
       const order = await Order.findOrCreate({
         where: {
-          userId: 3,
+          userId: userId,
           purchased: false
         }
       })
-      const { productId, quantity, price } = req.body
+      const { productId, quantity, price } = req.body.item
       const orderDetails = await Order_Detail.create({
         productId,
         quantity,
@@ -35,6 +40,23 @@ router.post('/', async (req, res, next) => {
         orderId: order[0].dataValues.id
       })
       res.status(201).json(orderDetails)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.destroy('/', async (req, res, next) => {
+  try {
+    const orderDetailId = req.body.id
+    const userId = req.user.id
+    const sessionId = req.body.userId
+    if (sessionId === userId) {
+      const deleteItem = await Order_Detail.destroy({
+        where: {
+          id: orderDetailId
+        }
+      })
     }
   } catch (err) {
     next(err)
--- server/api/checkout.js
+++ server/api/checkout.js
@@ -1,17 +1,36 @@
 const router = require('express').Router()
-const { Order } = require('../db/models')
+const { Order, Order_Detail } = require('../db/models')
 module.exports = router
 
-// if guest do a post???
+router.post('/', async (req, res, next) => {
+  try {
+    const guestOrder = await Order.create()
+    const orderId = guestOrder[0].dataValues.id
+    const guestCart = req.body.cart
+    guestCart.map(async product => {
+      const newOrderDetail = await Order_Detail.create({
+        productId: product.id,
+        quantity: product.quantity,
+        price: product.price,
+        orderId: orderId
+      })
+    })
+    res.status(201).json(guestOrder)
+  } catch (err) {
+    next(err)
+  }
+})
 
 router.put('/', async (req, res, next) => {
   try {
-    if (req.user) {
+    const sessionId = req.user.id
+    const userId = req.body.userId
+    if (sessionId === userId) {
       const checkout = await Order.update({
         purchased: true
       }, {
           where: {
-            userId: req.user.id
+            userId: userId
           }
         }
       )
