c4ada35afc76175e2affc91e5fd78b3b0764dbcb
Ryan McNierney <ryan.mcnierney@gmail.com>
2018-09-13 17:55:08 -0400

    Cart #Cart - set up cart conainer

---
+++ client/components/cart/cart-container.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CartContainer = () => {
+  return (
+    <div id="cart-container">
+      <h1>Cart Container</h1>
+      <h2>Sidebar</h2>
+    </div>
+  )
+}
+
+export default CartContainer
+
