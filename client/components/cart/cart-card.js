42ab6c1c6360709b197c4593e3c506da171ab0da
Ryan McNierney <ryan.mcnierney@gmail.com>
2018-09-14 09:46:51 -0400

    Cart-Card #Card - pulled in updateItemThunk from store

--- client/components/cart/cart-card.js
+++ client/components/cart/cart-card.js
@@ -1,37 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Panel, Image, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap'
+import { connect } from 'react-redux'
+import { updateItemInCartThunk } from '../../store/user'
 
 
-const CartCard = (props) => {
-  const { id, name, price, imageUrl } = props.item
-  return (
-    <Panel id="cart-card">
-      <Panel.Body>
-        <div id="cart-img">
-          <Image src={imageUrl} rounded />
-        </div>
-        <div id="card-info">
-          <div id="card-name-price">
-            <div id="card-name">{name}</div>
-            <div id="card-price">${price}</div>
+class CartCard extends Component {
+  render() {
+    const { id, name, price, imageUrl } = this.props.item
+    return (
+      <Panel id="cart-card">
+        <Panel.Body>
+          <div id="cart-img">
+            <Image src={imageUrl} rounded />
           </div>
-          <div id="card-qty-delete">
-            <FormGroup controlId="formControlsSelect">
-              <ControlLabel>Quantity</ControlLabel>
-              <FormControl componentClass="select" placeholder="select">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-              </FormControl>
-            </FormGroup>
-            <Button bsStyle="danger">Delete</Button>
+          <div id="card-info">
+            <div id="card-name-price">
+              <div id="card-name">{name}</div>
+              <div id="card-price">${price}</div>
+            </div>
+            <div id="card-qty-delete">
+              <FormGroup controlId="formControlsSelect">
+                <ControlLabel>Quantity</ControlLabel>
+                <FormControl componentClass="select" placeholder="select">
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </FormControl>
+              </FormGroup>
+              <Button bsStyle="danger">Delete</Button>
+            </div>
           </div>
-        </div>
-      </Panel.Body>
-    </Panel>
-  )
+        </Panel.Body>
+      </Panel>
+    )
+  }
 }
 
-export default CartCard
+const mapDispatch = dispatch => {
+  return {
+    updateCart: (item, quantity, user) => dispatch(updateItemInCartThunk(item, quantity, user))
+  }
+}
+
+export default connect(null, mapDispatch)(CartCard)
