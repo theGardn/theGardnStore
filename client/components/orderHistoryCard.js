90d779e5369a8e5bf6940eba5c6865633f620d28
Peeeeeng <40645138+Peeeeeng@users.noreply.github.com>
2018-09-17 00:17:08 -0400

    COMPONENT #PengComponent -create orderHistory

---
+++ client/components/orderHistoryCard.js
@@ -0,0 +1,55 @@
+import { Component } from 'react'
+import { connect } from 'react-redux'
+import { Panel } from 'react-bootstrap'
+
+
+export default class OrderHistoryCard extends Component {
+    constructor(props){
+        super(props)
+    }
+    render () {
+        const { order, eventKey } = this.props
+        let orderItemCount = 0
+        let totalPrice = 0
+        let purchasedItem
+        order.order_detail.forEach((item) => {
+            orderItemCount++
+            totalPrice += item.price
+            if(purchasedItem){
+                purchasedItem += ', ' + item.name
+            } else {
+                purchasedItem = item.name
+            }
+        })
+        let [orderDate, orderTime] = order.update_at.split(' ')
+        return (
+            <Panel eventKey={eventKey}>
+                <Panel.Heading>
+                    <Panel.Title toggle>
+                        <div className='rowContainer'>
+                            <div>
+                                <h4>Order#: {order.id}</h4>
+                            </div>
+                            <div>
+                                <h4>Purchased Item: {orderItemCount}</h4>
+                            </div>
+                            <div>
+                                <h4>Date: {orderDate}</h4>
+                            </div>
+                        </div>
+                    </Panel.Title>
+                </Panel.Heading>
+                <Panel.Body collapsible>
+                    <div className='rowContainer'>
+                        <div>
+                            <h5>Purchase Items: {purchasedItem}</h5>
+                        </div>
+                        <div>
+                            <h4>Total Price: {totalPrice}</h4>
+                        </div>
+                    </div>
+                </Panel.Body>
+            </Panel>
+        )
+    }
+}
