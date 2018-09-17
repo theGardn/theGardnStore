4c65f72b265f9e3ad5098f17c1a8b437ee2b4366
Ryan McNierney <ryan.mcnierney@gmail.com>
2018-09-17 18:40:58 -0400

    #OrderDetails - Hashed orderId for orderDetails page

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,11 +1,51 @@
+const crypto = require('crypto')
 const Sequelize = require('sequelize')
 const db = require('../db')
+
+let orderNum = 0;
+
+const generateOrderId = () => {
+  return 'order#' + orderNum++
+}
 
 const Order = db.define('order', {
   purchased: {
     type: Sequelize.BOOLEAN,
     defaultValue: false
+  },
+  orderId: {
+    type: Sequelize.STRING,
+    defaultValue: generateOrderId(),
+    get() {
+      return () => this.getDataValue('orderId')
+    }
+  },
+  salt: {
+    type: Sequelize.STRING,
+    get() {
+      return () => this.getDataValue('salt')
+    }
   }
 })
+
+Order.generateSalt = function () {
+  return crypto.randomBytes(16).toString('base64')
+}
+
+Order.encryptOrderId = function (plainText, salt) {
+  return crypto
+    .createHash('RSA-SHA256')
+    .update(plainText)
+    .update(salt)
+    .digest('hex')
+}
+
+const setSaltAndOrderId = order => {
+  order.salt = Order.generateSalt()
+  order.orderId = Order.encryptOrderId(order.orderId(), order.salt())
+}
+
+Order.beforeCreate(setSaltAndOrderId)
+
 
 module.exports = Order
